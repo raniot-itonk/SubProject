94a1ce895b0d65b5b9c10daea975d5dcd3a19864
Anders Meidahl <Andersmeidahl@gmail.com>
2019-03-17 11:50:14 +0100

    Updated so it is now ready to kube

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -10,45 +10,11 @@ namespace Backend
     {
         public static void Main(string[] args)
         {
-            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var configuration = GetConfiguration(env);
-            
-            SetupLogger(env, configuration);
-            try
-            {
-                Log.Information("Starting web host");
-                CreateWebHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Host terminated unexpectedly");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseSerilog();
-
-        private static void SetupLogger(string env, IConfiguration configuration)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .Enrich.WithProperty("Environment", env)
-                .ReadFrom.Configuration(configuration)
-                .CreateLogger();
-            Log.Information("Logger setup");
-        }
-
-        private static IConfigurationRoot GetConfiguration(string env)
-        {
-            return new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddJsonFile($"appsettings.{env}.json")
-                .Build();
-        }
+                .UseStartup<Startup>();
     }
 }
--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -41,20 +41,20 @@ namespace Backend
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, CraftsmanContext context)
         {
             app.UseAuthentication();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                context.Database.EnsureCreated();
             }
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                context.Database.Migrate();
             }
-
-            InitializeDatabase(app, env);
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
@@ -73,17 +73,6 @@ namespace Backend
             });
         }
 
-        private static void InitializeDatabase(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            using (var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
-            {
-                if(env.IsDevelopment())
-                    scope.ServiceProvider.GetRequiredService<CraftsmanContext>().Database.EnsureCreated();
-                else
-                    scope.ServiceProvider.GetRequiredService<CraftsmanContext>().Database.Migrate();
-
-            }
-        }
         private void SetupDatabase(IServiceCollection services, IHostingEnvironment env)
         {
             if(env.IsDevelopment())
